3e06f130a5968b1435d3dd6f7025df7ac7e8f899
Nilesh <nileshtupe31@gmail.com>
2018-06-12 00:02:37 +0530

    removed debugger statments

--- src/actions/ChatListActions.js
+++ src/actions/ChatListActions.js
@@ -4,10 +4,10 @@ import { FETCH_CHAT_LIST } from './types';
 export const fetchChatList = () => {
     return (dispatch) => {
       const { currentUser } = firebase.auth();
-      debugger;
       const phone = currentUser.email.replace('@chatapp.com', '');
       firebase.database().ref(`/chatApp/users/${phone}/chats`)
       .on('value', snapshot => {
+        debugger;
         dispatch({
           type: FETCH_CHAT_LIST,
           payload: snapshot.val()
